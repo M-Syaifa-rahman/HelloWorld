4ce59bc39604fa920bf1e96f478235e084d7635f
M-Syaifa-rahman <mhmdsyaifarahman@gmail.com>
2024-09-20 23:15:21 +0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * HelloWorld
+ * Nama: M Syaifa Rahman
+ * Nim : 2411016310003 
+ */ 
+public class HelloWorld {
+    public static void main(String[] args) {
+       // ctrl + shift + f: auto format
+       // sring menggunakan tanda petik 
+       System.out.println("Hello World");
+       System.out.println("nama:M Syaifa Rahman");
+       System.out.println("Nim :2411016310003");
+       
+       // angka (integer/DOUBLE/float) tidak perlu tanda petik
+       System.out.println(13);
+       System.out.println(true);
+
+       // char menggunakan tanda petik 1 ('')
+       System.out.println('A');
+    }   
+    
+}
